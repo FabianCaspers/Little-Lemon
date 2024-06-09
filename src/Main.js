cd88368bd43f328cff5361a33b25dc7f33c7003d
FabianCaspers <fabian.caspers1308@gmail.com>
2024-06-09 19:15:52 +0200

    add meta semantic og:

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <main>
+      {/* Hier können Sie Ihren Hauptinhalt hinzufügen */}
+    </main>
+  );
+}
+
+export default Main;
---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/home">Home</a></li>
+        <li><a href="/about">About Us</a></li>
+        <li><a href="/services">Service</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
