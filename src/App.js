aa6a2934f99dbd9766a0d46cda51ffcf454c821e
FabianCaspers <fabian.caspers1308@gmail.com>
2024-06-09 19:34:30 +0200

    add some styles

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <div className="Container">
       <Header />
       <Nav />
       <Main />
       <Footer />
-    </>
+    </div>
   );
 }
 
